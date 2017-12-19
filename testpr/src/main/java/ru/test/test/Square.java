e1935c9002623b4693db63996dcb87985eecb16f
Anton13447 <34650556+Anton13447@users.noreply.github.com>
2017-12-19 17:21:48 +0300

    Добавили 2 класса и сделали функции, ассоциированные с объектами

---
+++ testpr/src/main/java/ru/test/test/Square.java
@@ -0,0 +1,14 @@
+package ru.test.test;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
